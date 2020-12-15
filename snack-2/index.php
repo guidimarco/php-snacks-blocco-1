87f12aac1741618231a63a09deb2147d7b542f0c
Marco Guidi <mark.88g@gmail.com>
2020-12-15 14:05:00 +0100

    aggiunto check on mail && name

--- snack-2/index.php
+++ snack-2/index.php
@@ -1,12 +1,20 @@
 <?php
     // address-bar: ?name=pippo&mail=pippo@gmail.com&age=22
     $user_info = $_GET; // array of user's info
+    var_dump($user_info);
 
+
+    // name check
     if (strlen(trim($user_info["name"])) > 3) {
-        echo "maggiore di 3";
-    } else {
-        echo "minore o uguale di 3";
+        echo "nome OK";
     }
+
+    // mail check
+    if (strpos($user_info["mail"], ".") !== false && strpos($user_info["mail"], "@")) {
+        echo "mail OK";
+    }
+
+
 
 
 ?>
@@ -18,9 +26,9 @@
     </head>
     <body>
         <h1>Log-in</h1>
-        <p><?php var_dump($user_info) ?></p>
+        <!-- <p><?php var_dump($user_info) ?></p>
         <p><?php echo strlen($user_info["name"]) ?></p>
-        <p><?php echo strlen(trim($user_info["name"])) ?></p>
+        <p><?php echo strlen(trim($user_info["name"])) ?></p> -->
 
     </body>
 </html>
