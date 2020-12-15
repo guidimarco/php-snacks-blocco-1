ac5a42b6a57e2d6d516328a0b3c0432601efc52f
Marco Guidi <mark.88g@gmail.com>
2020-12-15 18:10:13 +0100

    snack-2 FINITO realizzata funzione di login e corrette alcune parti

--- snack-2/index.php
+++ snack-2/index.php
@@ -3,19 +3,34 @@
 
     // GET
     $user_info = $_GET; // array of user's info
-    var_dump($user_info);
+    // var_dump($user_info);
 
     // GLOBAL VAR
-    $logIn_msg = "";
+    $login_msg = ""; // string stamp on html
+
+
 
     // AGE CHECK, verify if it's a number
-    if (is_correct_name($user_info["name"]) && is_correct_mail($user_info["mail"]) && /* AGE CHECK */ is_numeric($user_info["age"])) {
-        $logIn_msg = "Accesso riuscito";
+    if (is_login_permitted($user_info["name"], $user_info["mail"], $user_info["age"])) {
+        $login_msg = "Accesso riuscito";
     } else {
-        $logIn_msg = "Accesso negato";
+        $login_msg = "Accesso negato";
     }
 
     // FUNCTION
+    function is_login_permitted($name, $mail, $age) {
+        // return true if is permetted
+        // condition: (1) name --> is_correct_name (2) mail --> is_correct_mail (3) age --> is_correct_age
+
+        // LOCAL VAR
+        $is_permitted = false;
+
+        if (is_correct_name($name) && is_correct_mail($mail) && is_correct_age($age)) {
+            $is_permitted = true;
+        };
+
+        return $is_permitted;
+    };
     function is_correct_name($name) {
         // return true if is correct
         // condition: 1) length is > min-charters
@@ -40,6 +55,17 @@
 
         return strpos($current_mail, ".") !== false && strpos($current_mail, "@") !== false;
     };
+    function is_correct_age($age) {
+        // return true if is correct
+        // condition: (1) is a number from min to max
+
+        // LOCAL VAR
+        $current_age = intval(trim($age)); // from string to int
+        $min = 18;
+        $max = 100;
+
+        return $current_age >= $min && $current_age <= $max;
+    };
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -53,7 +79,9 @@
         <p><?php echo strlen($user_info["name"]) ?></p>
         <p><?php echo strlen(trim($user_info["name"])) ?></p> -->
 
-        <p><?php echo $logIn_msg; ?></p>
+        <p>
+            <?php echo $login_msg; ?>
+        </p>
 
     </body>
 </html>
