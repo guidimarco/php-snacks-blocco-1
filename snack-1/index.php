b59d7685fcad68e8d43d611e74a92d2f3bcbd372
Marco Guidi <mark.88g@gmail.com>
2020-12-15 17:38:15 +0100

    snack-1 corretta str dati e funzione stampa

--- snack-1/index.php
+++ snack-1/index.php
@@ -3,47 +3,58 @@
     // GLOBAL VAR
     $matches = [
         [ // match 1
-            "vanoli basket cremona" => 80,
-            "openjobmetis varese" => 67
+            "home" => "vanoli basket cremona",
+            "home_score" => 80,
+            "vis" => "openjobmetis varese",
+            "vis_score" => 67
         ],
         [
-            "carpegna prosciutto pesaro" => 57,
-            "dolomiti energia trentino" => 71
+            "home" => "carpegna prosciutto pesaro",
+            "home_score" => 57,
+            "vis" => "dolomiti energia trentino",
+            "vis_score" => 71
         ],
         [
-            "banco di sardegna sassari" => 89,
-            "fortitudo lavoropiù bologna" => 86
+            "home" => "banco di sardegna sassari",
+            "home_score" => 89,
+            "vis" => "fortitudo lavoropiù bologna",
+            "vis_score" => 86
         ],
         [
-            "virtus segafredo bologna" => 67,
-            "unahotels reggio emilia" => 77
+            "home" => "virtus segafredo bologna",
+            "home_score" => 67,
+            "vis" => "unahotels reggio emilia",
+            "vis_score" => 77
         ],
         [
-            "acqua s. bernardo cantù" => 65,
-            "umana reyer venezia" => 75
+            "home" => "acqua s. bernardo cantù",
+            "home_score" => 65,
+            "vis" => "umana reyer venezia",
+            "vis_score" => 75
         ],
         [
-            "happy casa brindisi" => 99,
-            "de' longhi treviso basket" => 83
+            "home" => "happy casa brindisi",
+            "home_score" => 99,
+            "vis" => "de' longhi treviso basket",
+            "vis_score" => 83
         ],
         [
-            "germani brescia" => 75,
-            "allianz pallacanestro trieste" => 63
+            "home" => "germani brescia",
+            "home_score" => 75,
+            "vis" => "allianz pallacanestro trieste",
+            "vis_score" => 63
         ],
-    ]; // framework:: ["home_team" => score, "vis_team" => score], [...],
+    ]; // framework:: ["home" => "name team home", "home_score" => score_team_home, "vis" => "name team visitor", "vis_score" => score_team_vis], [...],
 
     // FUNCTION
     function stamp_matches($array_matches) {
         // stamp all the matches
 
         for ($i=0; $i < count($array_matches); $i++) {
-            // get current teams
-            $this_match_teams = array_keys($array_matches[$i]);
+            // get current match
+            $this_match = $array_matches[$i];
 
-            // get current scores
-            $this_match_scores = array_values($array_matches[$i]);
-
-            echo nl2br("\n" . ucwords($this_match_teams[0]) . " - " . ucwords($this_match_teams[1]) . " | " . $this_match_scores[0] . " - " . $this_match_scores[1]);
+            echo nl2br("\n" . ucwords($this_match["home"]) . " - " . ucwords($this_match["vis"]) . " | " . $this_match["home_score"] . " - " . $this_match["vis_score"]);
         }
     }
 ?>
